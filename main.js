5af54fb576f55813221430de66be11655e5617f6
Taylor Moe <78767930+taylormoe93@users.noreply.github.com>
2022-05-06 19:30:22 -0500

    Added javascript

---
+++ main.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var text = document.getElementById("skill-button");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
